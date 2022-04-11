e5c34064de4fac101c74c4d32637bf637b331b2c
each38527 <each38527@edu.ucl.dk>
2022-04-11 11:52:40 +0200

    Update clean.php

--- Renas/clean.php
+++ Renas/clean.php
@@ -33,8 +33,10 @@
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
                         <h2 class="pull-left">Dine rengøringer</h2>
-                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Ny rengøring</a>
+                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Ny rengøring</a> &nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href="index.php" class="btn btn-primary"> Tilbage til profil</a>
                     </div>
+
                     <?php
 
                     require_once "config.php";
@@ -82,5 +84,6 @@
         </div>
     </div>
 </body>
+
 
 </html>
